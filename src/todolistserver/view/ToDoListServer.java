b58136315870d416bade5ef55a564792faabd124
HimaAbousalem <hima.abousalem@gmail.com>
2019-12-30 23:25:16 +0200

    remove some useless comments

--- src/todolistserver/view/ToDoListServer.java
+++ src/todolistserver/view/ToDoListServer.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package todolistserver.view;
 
-import java.sql.Connection;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import todolistserver.model.DatabaseConnection;
 
 /**
  *
